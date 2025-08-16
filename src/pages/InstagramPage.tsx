f8f028c0ecf0ecb585adda0b87af7bf94f0bb19a
Ramos <77055236+ramadhan61@users.noreply.github.com>
2025-08-16 02:12:23 -0700

    Update InstagramPage.tsx

--- src/pages/InstagramPage.tsx
+++ src/pages/InstagramPage.tsx
@@ -37,7 +37,7 @@ const InstagramPage = () => {
       const response = await fetch(`https://api.paxsenix.biz.id/dl/aio?url=${encodeURIComponent(processedUrl)}`, {
         method: 'GET',
         headers: {
-          'Authorization': 'Bearer sk-paxsenix-cdiIbHKw5k-JQ70SC0IOaQNwsn8wk8hjU03Bi8M0DTw1NlqR',
+          'Authorization': 'Bearer sk-paxsenix-cdiIbHKw5k-JQ70SC0IOaQNwsn8wk8hjU03Bi8M0DTw1NlqR', //Change your api key
           'Content-Type': 'application/json'
         }
       });
