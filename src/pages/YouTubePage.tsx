f92b5007b96f4a54a1fd0c7fb69411def9098c46
Ramos <77055236+ramadhan61@users.noreply.github.com>
2025-08-16 02:11:24 -0700

    Update YouTubePage.tsx

--- src/pages/YouTubePage.tsx
+++ src/pages/YouTubePage.tsx
@@ -40,7 +40,7 @@ const YouTubePage = () => {
       const response = await fetch(`https://api.paxsenix.biz.id/dl/aio?url=${encodeURIComponent(processedUrl)}`, {
         method: 'GET',
         headers: {
-          'Authorization': 'Bearer sk-paxsenix-XzTVDzit2ZRHhOLkBuRJBU0OnYzMI6M5WXc2Zwnlzbb8LSOj',
+          'Authorization': 'Bearer sk-paxsenix-cdiIbHKw5k-JQ70SC0IOaQNwsn8wk8hjU03Bi8M0DTw1NlqR', //change your key
           'Content-Type': 'application/json'
         }
       });
